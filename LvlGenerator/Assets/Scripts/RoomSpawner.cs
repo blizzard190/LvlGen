ece918f10b8e19a8b683b210736b38354d224740
blizzard190 <22256@ma-web.nl>
2018-10-30 12:38:24 +0100

    Full

---
+++ LvlGenerator/Assets/Scripts/RoomSpawner.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour {
+
+    public int openingDirection;
+    public bool spawned = false;
+    public float waitTime = 4f;
+
+    private RoomTemplates templates;
+	private int rand;
+
+	void Start(){
+		Destroy(gameObject, waitTime);
+		templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+		Invoke("Spawn", 0.1f);
+	}
+
+
+	void Spawn(){
+		if(spawned == false)
+        {
+            switch (openingDirection)
+            {
+                case 1:
+                    rand = Random.Range(0, templates.bottomRooms.Length);
+                    Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
+                    break;
+                case 2:
+                    rand = Random.Range(0, templates.topRooms.Length);
+                    Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
+                    break;
+                case 3:
+                    rand = Random.Range(0, templates.leftRooms.Length);
+                    Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
+                    break;
+                case 4:
+                    rand = Random.Range(0, templates.rightRooms.Length);
+                    Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
+                    break;
+            }
+            spawned = true;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.CompareTag("SpawnPoint")){
+			if(other.GetComponent<RoomSpawner>().spawned == false && spawned == false){
+				Instantiate(templates.closedRoom, transform.position, Quaternion.identity);
+				Destroy(gameObject);
+			} 
+			spawned = true;
+		}
+	}
+}
